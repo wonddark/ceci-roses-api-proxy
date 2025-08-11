e1216cb3df6b8fb7a74caa8d0c9f2992d8b2316d
Osmanys Fuentes L <ossmanys@gmail.com>
2025-08-11 12:39:50 +0400

    Using the request headers to set proxy headers instead of hardcoding

--- src/app/api/Admin/getMenusBy/route.ts
+++ src/app/api/Admin/getMenusBy/route.ts
@@ -1,33 +1,20 @@
-import { cookies } from "next/headers";
-
 export async function POST(req: Request) {
-  const reqCookies = await cookies();
-  const session = reqCookies.get("session");
-  if (session) {
-    const parsed = JSON.parse(session.value);
-    const token = parsed.user.token;
+  const reqHeaders = req.headers;
+  const proxyURL = new URL("/api/Admin/getMenusBy", process.env.API_URL);
+  const proxyRequest = new Request(proxyURL, {
+    body: req.body,
+    method: "POST",
+    headers: reqHeaders,
+    // @ts-expect-error Unknown property
+    duplex: "half",
+  });
 
-    const proxyURL = new URL("/api/Admin/getMenusBy", process.env.API_URL);
-    const proxyRequest = new Request(proxyURL, {
-      body: req.body,
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      // @ts-expect-error Unknown property
-      duplex: "half",
-    });
+  try {
+    return fetch(proxyRequest);
+  } catch (reason) {
+    const message =
+      reason instanceof Error ? reason.message : "Unexpected exception";
 
-    try {
-      return fetch(proxyRequest);
-    } catch (reason) {
-      const message =
-        reason instanceof Error ? reason.message : "Unexpected exception";
-
-      return new Response(message, { status: 500 });
-    }
+    return new Response(message, { status: 500 });
   }
-
-  return new Response("No session found", { status: 401 });
 }
