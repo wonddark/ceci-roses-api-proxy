fcb93ddc3012585eb1238055b5125aa48fed586f
Osmanys Fuentes L <ossmanys@gmail.com>
2025-08-11 12:18:43 +0400

    Route for proxying 'Account/register'

---
+++ src/app/api/Account/register/route.ts
@@ -0,0 +1,19 @@
+export async function POST(req: Request) {
+  const proxyURL = new URL("/api/Account/register", process.env.API_URL);
+  const proxyRequest = new Request(proxyURL, {
+    body: req.body,
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    // @ts-expect-error Unknown property
+    duplex: "half",
+  });
+
+  try {
+    return fetch(proxyRequest);
+  } catch (reason) {
+    const message =
+      reason instanceof Error ? reason.message : "Unexpected exception";
+
+    return new Response(message, { status: 500 });
+  }
+}
