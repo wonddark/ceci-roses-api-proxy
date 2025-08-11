702b3c2c840d2ca44cbc7a476544b1bb47e1277f
Osmanys Fuentes L <ossmanys@gmail.com>
2025-08-11 19:34:33 +0400

    Added endpoints /orders and /orders/userOrders

---
+++ src/app/api/orders/userOrders/route.ts
@@ -0,0 +1,13 @@
+export async function GET(req: Request) {
+  const proxyURL = new URL("/api/userOrders", process.env.API_URL);
+  const proxyRequest = new Request(proxyURL, req);
+
+  try {
+    return fetch(proxyRequest);
+  } catch (reason) {
+    const message =
+      reason instanceof Error ? reason.message : "Unexpected exception";
+
+    return new Response(message, { status: 500 });
+  }
+}
