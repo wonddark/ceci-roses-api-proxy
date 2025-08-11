cdcbe5c120067154e0390f095034c9edef313653
Osmanys Fuentes L <ossmanys@gmail.com>
2025-08-11 12:42:21 +0400

    Route for proxying 'Admin'

---
+++ src/app/api/Admin/route.ts
@@ -0,0 +1,19 @@
+export async function POST(req: Request) {
+  const proxyURL = new URL("/api/Admin", process.env.API_URL);
+  const proxyRequest = new Request(proxyURL, {
+    body: req.body,
+    method: "POST",
+    headers: req.headers,
+    // @ts-expect-error Unknown property
+    duplex: "half",
+  });
+
+  try {
+    return fetch(proxyRequest);
+  } catch (reason) {
+    const message =
+      reason instanceof Error ? reason.message : "Unexpected exception";
+
+    return new Response(message, { status: 500 });
+  }
+}
