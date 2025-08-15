4e0944c2a82cf6536c20221ef96167df25dd70a2
Osmanys Fuentes L <ossmanys@gmail.com>
2025-08-15 11:22:00 +0400

    Simplified the logic of proxy geMenusFilterBy

--- src/app/api/MenuPages/getMenusFilterBy/route.ts
+++ src/app/api/MenuPages/getMenusFilterBy/route.ts
@@ -7,12 +7,5 @@ export async function POST(req: NextRequest) {
   );
   const proxyRequest = new Request(proxyURL, req);
 
-  try {
-    return fetch(proxyRequest);
-  } catch (reason) {
-    const message =
-      reason instanceof Error ? reason.message : "Unexpected exception";
-
-    return new Response(message, { status: 500 });
-  }
+  return fetch(proxyRequest);
 }
