d250aa48aa27ef4ef5cc19da1ee34d43deeb76f2
Osmanys Fuentes L <ossmanys@gmail.com>
2025-08-15 09:24:24 +0400

    Manually sending the desired id
    It seems the cloned request does not come with the search param

--- src/app/api/Account/getById/route.ts
+++ src/app/api/Account/getById/route.ts
@@ -1,5 +1,11 @@
-export async function GET(req: Request) {
-  const proxyURL = new URL("/api/Account/getById", process.env.API_URL);
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const id = req.nextUrl.searchParams.get("id");
+  const proxyURL = new URL(
+    `/api/Account/getById?id=${id}`,
+    process.env.API_URL,
+  );
   const proxyRequest = new Request(proxyURL, req);
 
   return fetch(proxyRequest);
