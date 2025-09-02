9cc1a5cc778c27528abc445618900b8c025e1d0f
Osmanys Fuentes L <ossmanys@gmail.com>
2025-09-02 13:30:25 +0400

    Log cloned request

--- src/app/api/orders/userOrders/route.ts
+++ src/app/api/orders/userOrders/route.ts
@@ -1,6 +1,7 @@
 export async function GET(req: Request) {
   const proxyURL = new URL("/api/orders/userOrders", process.env.API_URL);
   const proxyRequest = new Request(proxyURL, req);
+  console.log(JSON.stringify(proxyRequest, null, 2));
 
   return fetch(proxyRequest);
 }
