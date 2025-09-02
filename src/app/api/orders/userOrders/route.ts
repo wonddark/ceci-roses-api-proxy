0825e376b377d020473a8d95cf6b413a9f981fc3
Osmanys Fuentes L <ossmanys@gmail.com>
2025-09-02 15:09:14 +0400

    Removed a log to console

--- src/app/api/orders/userOrders/route.ts
+++ src/app/api/orders/userOrders/route.ts
@@ -1,7 +1,6 @@
 export async function GET(req: Request) {
   const proxyURL = new URL("/api/orders/userOrders", process.env.API_URL);
   const proxyRequest = new Request(proxyURL, req);
-  console.log(JSON.stringify(proxyRequest, null, 2));
 
   return fetch(proxyRequest);
 }
