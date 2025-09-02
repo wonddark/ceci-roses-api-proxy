64522bb85d38c218ec65fb64ebb45cb34c0f416d
Osmanys Fuentes L <ossmanys@gmail.com>
2025-09-02 10:58:50 +0400

    Added route to /Reviews/google-reviews

---
+++ src/app/api/Reviews/google-reviews/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams;
+  const proxyURL = new URL(
+    `/api/Reviews/google-reviews?${query}`,
+    process.env.API_URL,
+  );
+  const proxyRequest = new Request(proxyURL, req);
+
+  return fetch(proxyRequest);
+}
