74579dc84eca48c93fa65f268d73442684d62cdc
Osmanys Fuentes L <ossmanys@gmail.com>
2025-09-05 14:48:43 +0400

    Fixed the proxy to /notifications/subscribe
    Added the query var for email

--- src/app/api/notifications/subscribe/route.ts
+++ src/app/api/notifications/subscribe/route.ts
@@ -1,5 +1,11 @@
-export async function POST(req: Request) {
-  const proxyURL = new URL("/api/notifications/subscribe", process.env.API_URL);
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const email = req.nextUrl.searchParams.get("email");
+  const proxyURL = new URL(
+    `/api/notifications/subscribe?email=${email}`,
+    process.env.API_URL,
+  );
   const proxyRequest = new Request(proxyURL, req);
 
   return fetch(proxyRequest);
