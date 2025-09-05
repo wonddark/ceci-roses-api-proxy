534017e3e89b8195f05b388120755adc5647e341
Osmanys Fuentes L <ossmanys@gmail.com>
2025-09-05 15:03:51 +0400

    Fixed the proxy to /notifications/contact
    Added the query var for current language

--- src/app/api/notifications/contact/route.ts
+++ src/app/api/notifications/contact/route.ts
@@ -1,5 +1,11 @@
-export async function POST(req: Request) {
-  const proxyURL = new URL("/api/notifications/contact", process.env.API_URL);
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const languageCode = req.nextUrl.searchParams.get("languageCode");
+  const proxyURL = new URL(
+    `/api/notifications/contact?languageCode=${languageCode}`,
+    process.env.API_URL,
+  );
   const proxyRequest = new Request(proxyURL, req);
 
   return fetch(proxyRequest);
