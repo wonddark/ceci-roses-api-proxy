c7d9ab6faed9fd633836271ca1255a90b58e4f22
Osmanys Fuentes L <ossmanys@gmail.com>
2025-09-24 14:40:10 +0400

    Updated route /users allowing to send queries

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,8 @@
-export async function GET(req: Request) {
-  const proxyURL = new URL("/api/users", process.env.API_URL);
+import { NextRequest } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const query = req.nextUrl.searchParams;
+  const proxyURL = new URL(`/api/users?${query}`, process.env.API_URL);
   const proxyRequest = new Request(proxyURL, req);
 
   return fetch(proxyRequest);
