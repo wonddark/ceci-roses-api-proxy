e4180888023665b6f09c9ac343f34d0af4325e52
Osmanys Fuentes L <ossmanys@gmail.com>
2026-02-04 10:27:23 +0400

    Updated /api/Admin proxy to add CORS headers

--- src/app/api/Admin/route.ts
+++ src/app/api/Admin/route.ts
@@ -2,5 +2,9 @@ export async function POST(req: Request) {
   const proxyURL = new URL("/api/Admin", process.env.API_URL);
   const proxyRequest = new Request(proxyURL, req);
 
-  return fetch(proxyRequest);
+  const res = await fetch(proxyRequest);
+
+  res.headers.set("Access-Control-Allow-Origin", "*");
+
+  return res;
 }
