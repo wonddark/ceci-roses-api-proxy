bb04ff2793ca8e0f9538e979c124e30462f9f592
Osmanys Fuentes L <ossmanys@gmail.com>
2026-02-04 10:55:12 +0400

    Updated tsconfig.json: switched JSX mode to react-jsx, adjusted paths and includes/excludes; fixed type of `id` param in /orders/getById/[id] route

--- src/app/api/orders/getById/[id]/route.ts
+++ src/app/api/orders/getById/[id]/route.ts
@@ -1,6 +1,6 @@
 export async function GET(
   req: Request,
-  { params }: { params: Promise<{ id: number }> },
+  { params }: { params: Promise<{ id: string }> },
 ) {
   const { id } = await params;
   const proxyURL = new URL(`/api/orders/getById/${id}`, process.env.API_URL);
