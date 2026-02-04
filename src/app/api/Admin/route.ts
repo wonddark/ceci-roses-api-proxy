6fc103ac5f227ac9dd9adda3873d88a5be3eaa39
Osmanys Fuentes L <ossmanys@gmail.com>
2026-02-04 11:19:55 +0400

    Simplified /api/Admin proxy by removing CORS headers

--- src/app/api/Admin/route.ts
+++ src/app/api/Admin/route.ts
@@ -2,9 +2,5 @@ export async function POST(req: Request) {
   const proxyURL = new URL("/api/Admin", process.env.API_URL);
   const proxyRequest = new Request(proxyURL, req);
 
-  const res = await fetch(proxyRequest);
-
-  return new Response(res.body, {
-    headers: { "Access-Control-Allow-Origin": "*" },
-  });
+  return fetch(proxyRequest);
 }
